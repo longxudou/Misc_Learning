003f71dbb4923b3106624500c31692e24890ea3f
longxud <longxud@hit.edu.cn>
2019-02-26 20:37:36 +0800

    add preorder/inorder/postorder traversal in BiTree iteratively

--- Interview/BiTree.py
+++ Interview/BiTree.py
@@ -12,6 +12,8 @@ def preTravese(root):
     preTravese(root.left)
     preTravese(root.right)
 
+
+
 root=Node('D',Node('E',Node('G',left=Node('F'))),Node('B',Node('C'),Node('A')))
 # root=Node('D')
 
@@ -40,7 +42,8 @@ findTree(preList,midList,afterList)
 print afterList
 
 
-class Solution(object):
+
+class BST_Solution(object):
     def buildTree_pre(self, preorder, inorder):
         if not inorder or not preorder:
             return None
@@ -55,7 +58,7 @@ class Solution(object):
         return root
 
     def buildTree_post(self, inorder, postorder):
-        
+
         if not inorder or not postorder:
             return None
 
@@ -68,3 +71,50 @@ class Solution(object):
         root.left = self.buildTree(inorder[:root_index], postorder)
 
         return root
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        stack = []
+        ans = []
+        node = root
+
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                ans.append(node.val)
+                node = node.right
+        return ans
+
+    def preorderTraversal(self, root):
+        ans = []
+        stack = []
+        node = root
+
+        while stack or node:
+            if node:
+                stack.append(node)
+                ans.append(node.val)
+                node = node.left
+            else:
+                node = stack.pop()
+                node = node.right
+
+        return ans
+
+    def postorderTraversal(self, root):
+        ans = []
+        stack = []
+        node = root
+        while stack or node:
+            if node:
+                stack.append(node)
+                ans.insert(0, node.val)
+                node = node.right
+            else:
+                node = stack.pop()
+                node = node.left
+        return ans
