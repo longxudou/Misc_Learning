b77baf353dba4401179a0c21e4f921fdac6ab93f
longxud <longxud@hit.edu.cn>
2019-03-12 23:49:06 +0800

    modified sorted_list

---
+++ Interview/Sorted_List.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        tmp_ptr = head
+        tmp_val = tmp_ptr.val
+
+        tmp2 = head
+        while (tmp2.next):
+
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = json.loads(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+
+def main():
+
+    head = stringToListNode([4,2,1,3])
+
+    ret = Solution().sortList(head)
+
+    out = listNodeToString(ret)
+    print out
+
+
+
+if __name__ == '__main__':
+    main()
