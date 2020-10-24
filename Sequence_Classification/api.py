279003a24e97a2bc4aa991aeeaa546e766a14db6
longxud <longxud@hit.edu.cn>
2020-10-24 16:26:43 +0800

    init api query

---
+++ Sequence_Classification/api.py
@@ -0,0 +1,41 @@
+# encoding:utf-8
+import requests
+import json
+
+def get_access_token():
+    # client_id 为官网获取的AK， client_secret 为官网获取的SK
+    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=RiqnlGlCco6lwYfyz5s9MdeA&client_secret=eHfNKIaS9tM533OBcUDrSpfl76rywWXk'
+
+    response = requests.get(host)
+    # if response:
+    #     print(response.json())
+    content = response.json()
+    access_token = content["access_token"]
+    return access_token
+
+
+def query_one(data,access_token):
+    result=[]
+    request_url = "https://aip.baidubce.com/rpc/2.0/ai_custom_pro/v1/text_cls/mooc_v1" + "?access_token=" + access_token
+
+    for token in data:
+        query={'text':token}
+        response = requests.post(request_url, data=json.dumps(query))
+        content = response.json()
+
+        tag=content['results'][0]['name']
+        score = content['results'][0]['score']
+
+        result.append((tag,score))
+    return content
+
+def query(input_token_list):
+    access_token=get_access_token()
+    result=query_one(input_token_list,access_token)
+    return result
+
+if __name__ == '__main__':
+    input_token_list=['我们说采购管理的一个基本的思想是什么？']
+    result=query(input_token_list)
+    print(result)
+
--- Sequence_Classification/divide.py
+++ Sequence_Classification/divide.py
@@ -1,5 +1,6 @@
 import re,json,os, argparse
-
+from collections import Counter
+from api import query
 TIME_INTERVAL=30
 
 def divide(path):
@@ -21,43 +22,53 @@ def divide(path):
         end=int(lines[index*3+1][23:25])
         sentence=lines[index*3+2]
 
-        print(start,end)
+        # print(start,end)
 
         # 0, start, end , 30
-        if 0<start<=end<30:
-            if index==0 or int(lines[index*3-2][23:25])<30:
+        if 0<start<=end<TIME_INTERVAL:
+            if index==0 or int(lines[index*3-2][23:25])<TIME_INTERVAL:
                 tmp.append(sentence)
             else:
                 collect.append(tmp)
                 tmp=[sentence]
 
         # 0, start, 30, end
-        elif 0<start<30<end:
+        elif 0<start<TIME_INTERVAL<end:
             tmp.append(sentence)
             collect.append(tmp)
             tmp=[]
 
         # 0, 30, start, 0, end
-        elif 30<start and 0<end<30:
+        elif TIME_INTERVAL<start and 0<end<TIME_INTERVAL:
             tmp.append(sentence)
             collect.append(tmp)
             tmp=[]
 
         # 0, 30, start, end
-        elif 30<start<=end:
-            if index == 0 or int(lines[index * 3 - 2][6:8]) > 30:
+        elif TIME_INTERVAL<start<=end:
+            if index == 0 or int(lines[index * 3 - 2][6:8]) > TIME_INTERVAL:
                 tmp.append(sentence)
             else:
                 collect.append(tmp)
                 tmp=[sentence]
 
-
     if tmp!=[]:
         collect.append(tmp)
     return collect
 
+def set_tag_for_interval(list_of_interval):
+    result = []
+    for list_of_sentence in list_of_interval[:2]:
+        per_sentence_tag=query(list_of_sentence)
+        tag_frequence=Counter([i['name'] for i in per_sentence_tag['results']])
+        result.append(tag_frequence.most_common(1)[0][0])
+
+
+    return result
 
 if __name__ == '__main__':
     path_list=['/Users/longxud/Downloads/time/2020-03-10.srt']
     for file_path in path_list:
         collect=divide(file_path)
+        result=set_tag_for_interval(collect)
+        print(result)
